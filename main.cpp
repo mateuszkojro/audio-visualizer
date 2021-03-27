98a3c22e0d7053691d26add1e1859a1e8c66f8fa
Mateusz Kojro <mateuszkojro@outlook.com>
2021-03-27 13:57:38 +0100

    Adding simple example from https://gist.github.com/armornick/3447121

---
+++ main.cpp
@@ -0,0 +1,98 @@
+#include <SDL.h>
+#include <cstdio>
+
+#define MUS_PATH "../example.wav"
+#define LOAD_WAV true
+
+//--------------------------------------------------------
+// example from: https://gist.github.com/armornick/3447121
+//--------------------------------------------------------
+
+// prototype for our audio callback
+// see the implementation for more information
+void my_audio_callback(void *userdata, Uint8 *stream, int len);
+
+// variable declarations
+static Uint8 *audio_pos; // global pointer to the audio buffer to be played
+static Uint32 audio_len; // remaining length of the sample we have to play
+
+
+/*
+** PLAYING A SOUND IS MUCH MORE COMPLICATED THAN IT SHOULD BE
+*/
+int main(int argc, char* argv[]){
+
+    // Initialize SDL.
+    if (SDL_Init(SDL_INIT_AUDIO) < 0){
+        printf("SDL init error\n");
+        return 1;
+    }
+
+
+    // local variables
+    static Uint32 wav_length; // length of our sample
+    static Uint8 *wav_buffer; // buffer containing our audio file
+    static SDL_AudioSpec wav_spec; // the specs of our piece of music
+
+
+#ifdef LOAD_WAV
+    /* Load the WAV */
+    // the specs, length and buffer of our wav are filled
+    if( SDL_LoadWAV(MUS_PATH, &wav_spec, &wav_buffer, &wav_length) == NULL ){
+        printf("Coud not load audio\n");
+        return 1;
+    }
+#endif
+
+
+
+    // set the callback function
+    wav_spec.callback = my_audio_callback;
+    wav_spec.userdata = NULL;
+    // set our global static variables
+    audio_pos = wav_buffer; // copy sound buffer
+    audio_len = wav_length; // copy file length
+
+    /* Open the audio device */
+    if ( SDL_OpenAudio(&wav_spec, NULL) < 0 ){
+        fprintf(stderr, "Couldn't open audio: %s\n", SDL_GetError());
+        exit(-1);
+    }
+
+    /* Start playing */
+    SDL_PauseAudio(0);
+
+    // wait until we're don't playing
+    while ( audio_len > 0 ) {
+        SDL_Delay(100);
+    }
+
+    // shut everything down
+    SDL_CloseAudio();
+    SDL_FreeWAV(wav_buffer);
+
+    return 0;
+}
+
+// audio callback function
+// here you have to copy the data of your audio buffer into the
+// requesting audio buffer (stream)
+// you should only copy as much as the requested length (len)
+void my_audio_callback(void *userdata, Uint8 *stream, int len) {
+
+    if (audio_len ==0)
+        return;
+
+    int i = len;
+    printf("Do skopiowania: %d\n",len);
+    while (i--){
+        stream[i] = sin(i);
+    }
+
+    len = ( len > audio_len ? audio_len : len );
+    SDL_memcpy (stream, audio_pos, len); 					// simply copy from one buffer into the other
+    //SDL_MixAudio(stream, audio_pos, len, SDL_MIX_MAXVOLUME);// mix from one buffer into another
+
+    audio_pos += len;
+    audio_len -= len;
+}
