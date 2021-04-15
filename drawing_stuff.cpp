7065ef3bd7fe98af76c347387143400672afb92c
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-04-15 04:18:14 +0200

    replaced whole main with mine, right now it does waves (yey), nex step is to make it represent something a.k.a. show current volume levels at different frequency's. Also it would be nice to be able to modify our graph with mouse, currently it is done by arrow keys. At some point we need to think on some thread locks or mutex to be able to send and receive information to and from threads.

---
+++ drawing_stuff.cpp
@@ -0,0 +1,128 @@
+//
+// Created by studio25 on 14.04.2021.
+//
+
+#include "drawing_stuff.h"
+
+
+rgb_color gen_rainbow(unsigned height, unsigned max_height) {
+    // sine wave algorythm
+    // 3 parts
+    // 1 :
+    // r = cos(i) , g = sin(i), b =0
+    // 2 :
+    // r = 0 , g = cos(i), b = sin(x)
+    // 3:
+    // r = sin(i) , g = 0, b = cos(x)
+    // every part is max_height / 3 translates into 0, PI/2
+
+    // so for example in point 1/3 * max_height
+    // r = cos(PI/2) = 0, g = sin(PI/2) = 1, b = 0
+
+    char witch_third = height / (max_height / 3);
+
+    double height_in_radians;
+    switch (witch_third) {
+        case 0:
+            height_in_radians = height * PI / (max_height / 3) / 2;
+
+            return {cos(height_in_radians) * 255, sin(height_in_radians) * 255, 0};
+        case 1:
+            height -= max_height / 3;
+            height_in_radians = height * PI / (max_height / 3) / 2;
+            return {0, cos(height_in_radians) * 255, sin(height_in_radians) * 255};
+
+        case 2:
+            height -= 2 * max_height / 3;
+            height_in_radians = height * PI / (max_height / 3) / 2;
+            return {sin(height_in_radians) * 255, 0, cos(height_in_radians) * 255};
+
+    }
+
+
+}
+void draw_circle(SDL_Renderer *renderer, cord &point, int R){
+
+    // following (x+a)^2 + (y+b)^2 = r^2
+    //
+
+    int x = point.x - R - 1; // tweleve'o clock
+                             // -1 is there to stay true to the rule
+                             // basically point is a circle with R = 1
+                             // so the dot is made at point
+                             // soo we subtract 1 because R.. ehh you get it
+    int y = point.y;
+
+    for(int i=0;i<R*R;i++){
+        SDL_RenderDrawPoint(renderer, point.x-cos(PI/(2*R)), y+-sin(PI/(2*R)));
+    }
+
+
+
+
+
+}
+
+void draw_point(SDL_Renderer *renderer, cord &point) {
+    //set color
+    rgb_color current_pixel_color = gen_rainbow(point.y, WINDOW_HEIGHT);
+
+    SDL_SetRenderDrawColor(renderer, current_pixel_color.r, current_pixel_color.g,
+                           current_pixel_color.b, 255);
+    //center
+    SDL_RenderDrawPoint(renderer, point.x, point.y);
+    // up
+    SDL_RenderDrawPoint(renderer, point.x, point.y - 2);
+    SDL_RenderDrawPoint(renderer, point.x, point.y - 1);
+
+    //down
+    SDL_RenderDrawPoint(renderer, point.x, point.y + 1);
+    SDL_RenderDrawPoint(renderer, point.x, point.y + 2);
+    // left
+    SDL_RenderDrawPoint(renderer, point.x - 2, point.y);
+    SDL_RenderDrawPoint(renderer, point.x - 1, point.y);
+    //right
+    SDL_RenderDrawPoint(renderer, point.x + 1, point.y);
+    SDL_RenderDrawPoint(renderer, point.x + 2, point.y);
+    // corners
+    SDL_RenderDrawPoint(renderer, point.x - 1, point.y - 1);
+    SDL_RenderDrawPoint(renderer, point.x - 1, point.y + 1);
+    SDL_RenderDrawPoint(renderer, point.x + 1, point.y + 1);
+    SDL_RenderDrawPoint(renderer, point.x + 1, point.y - 1);
+
+}
+
+void draw_big_point(SDL_Renderer *renderer, cord &point, int r) {
+    //set color
+    rgb_color current_pixel_color = gen_rainbow(point.y, WINDOW_HEIGHT);
+
+    SDL_SetRenderDrawColor(renderer, current_pixel_color.r, current_pixel_color.g,
+                           current_pixel_color.b, 255);
+    //center
+    SDL_RenderDrawPoint(renderer, point.x, point.y);
+    for (int i = 1; i <= r; i++) {
+        // up
+
+        SDL_RenderDrawPoint(renderer, point.x, point.y - i);
+
+        //down
+
+        SDL_RenderDrawPoint(renderer, point.x, point.y + i);
+
+        // left
+
+        SDL_RenderDrawPoint(renderer, point.x - i, point.y);
+
+        //right
+
+        SDL_RenderDrawPoint(renderer, point.x + i, point.y);
+    }
+    // corners
+    for (int i = 1; i <= r / 2; i++) {
+        SDL_RenderDrawPoint(renderer, point.x - i, point.y - i);
+        SDL_RenderDrawPoint(renderer, point.x - i, point.y + i);
+        SDL_RenderDrawPoint(renderer, point.x + i, point.y + i);
+        SDL_RenderDrawPoint(renderer, point.x + i, point.y - i);
+    }
+}
+
--- main.cpp
+++ main.cpp
@@ -1,92 +1,278 @@
+//
+// Created by studio25 on 07.04.2021.
+//
+#include <iostream>
+#include <thread>
+#include <cmath>
+#include <conio.h>
+#include <vector>
 #include <SDL.h>
-#include <cstdio>
+#include "drawing_stuff.h"
 
-#define MUS_PATH "./example.wav"
-#define LOAD_WAV true
-
-//--------------------------------------------------------
-// example from: https://gist.github.com/armornick/3447121
-//--------------------------------------------------------
-
-// prototype for our audio callback
-// see the implementation for more information
-void my_audio_callback(void *userdata, Uint8 *stream, int len);
-
-// variable declarations
-static Uint8 *audio_pos; // global pointer to the audio buffer to be played
-static Uint32 audio_len; // remaining length of the sample we have to play
-
-
-/*
-** PLAYING A SOUND IS MUCH MORE COMPLICATED THAN IT SHOULD BE
-*/
-int main(int argc, char* argv[]){
-
-    // Initialize SDL.
-    if (SDL_Init(SDL_INIT_AUDIO) < 0){
-        printf("SDL init error\n");
-        return 1;
-    }
-
-
-    // local variables
-    static Uint32 wav_length; // length of our sample
-    static Uint8 *wav_buffer; // buffer containing our audio file
-    static SDL_AudioSpec wav_spec; // the specs of our piece of music
-
-
-#ifdef LOAD_WAV
-    /* Load the WAV */
-    // the specs, length and buffer of our wav are filled
-    if( SDL_LoadWAV(MUS_PATH, &wav_spec, &wav_buffer, &wav_length) == NULL ){
-        printf("Coud not load audio\n");
-        return 1;
-    }
-#endif
+#define WINDOW_WIDTH 1200
+#define WINDOW_HEIGHT 600
+#define PI 3.1415
 
 
 
-    // set the callback function
-    wav_spec.callback = my_audio_callback;
-    wav_spec.userdata = NULL;
-    // set our global static variables
-    audio_pos = wav_buffer; // copy sound buffer
-    audio_len = wav_length; // copy file length
 
-    /* Open the audio device */
-    if ( SDL_OpenAudio(&wav_spec, NULL) < 0 ){
-        fprintf(stderr, "Couldn't open audio: %s\n", SDL_GetError());
-        exit(-1);
-    }
+// todo 1) make them graphs represent something
+// todo 2) make the mouse comprtoller
 
-    /* Start playing */
-    SDL_PauseAudio(0);
+std::vector<double> interpolate_your_ass_you_bitch(cord begin, cord end, std::vector<cord> data){
 
-    // wait until we're don't playing
-    while ( audio_len > 0 ) {
-        SDL_Delay(100);
-    }
+    // number of points
+    int p_number = 2 + data.size();
+    int polynomial_degree = p_number-1;
 
-    // shut everything down
-    SDL_CloseAudio();
-    SDL_FreeWAV(wav_buffer);
+    // so my polynomial will simply be vector of coresponding things
+    std::vector<double> function;
 
-    return 0;
+    return function;
 }
 
-// audio callback function
-// here you have to copy the data of your audio buffer into the
-// requesting audio buffer (stream)
-// you should only copy as much as the requested length (len)
-void my_audio_callback(void *userdata, Uint8 *stream, int len) {
 
-    if (audio_len ==0)
-        return;
 
-    len = ( len > audio_len ? audio_len : len );
-    SDL_memcpy (stream, audio_pos, len); 					// simply copy from one buffer into the other
-    //SDL_MixAudio(stream, audio_pos, len, SDL_MIX_MAXVOLUME);// mix from one buffer into another
+enum key_pressed {
+    up,
+    down,
+    left,
+    right,
+    space,
+    none
 
-    audio_pos += len;
-    audio_len -= len;
+
+};
+
+
+std::vector<cord> data;
+
+
+int window_with_line(void *ptr_to_data) {
+
+    SDL_Event event;
+    SDL_Renderer *renderer;
+    SDL_Window *window;
+
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer);
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+    for (int i = 0; i < WINDOW_WIDTH; ++i)
+        SDL_RenderDrawPoint(renderer, i, WINDOW_HEIGHT / 2);
+    SDL_RenderPresent(renderer); // starting line
+
+    auto time_start = std::chrono::steady_clock::now();
+
+
+    double amplituda = 150;
+    double okres = 0.5;
+    int k = 0;
+    int middle = WINDOW_HEIGHT / 2;
+
+    key_pressed current_key = none;
+    while (true) {
+        if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
+            break;
+        if (event.type == SDL_KEYDOWN) {
+            switch (event.key.keysym.sym) {
+                case SDLK_UP:
+                    if (current_key != up) amplituda += 1.1;
+                    current_key = up;
+                    break;
+
+                case SDLK_DOWN:
+                    if (current_key != down) amplituda -= 1.1;
+                    current_key = down;
+                    break;
+
+                case SDLK_LEFT:
+                    if (current_key != left) okres -= 0.01;
+                    current_key = left;
+                    break;
+
+                case SDLK_RIGHT:
+                    if (current_key != right) okres += 0.01;
+                    current_key = right;
+                    break;
+                case SDLK_SPACE:
+                    if (current_key != space) {
+                        std::cout << "amplituda: " << amplituda << "\t" << "okres: " << okres << "\n";
+                    }
+                    current_key = space;
+                    break;
+                default:
+                    current_key = none;
+                    break;
+            }
+        }
+        {
+            auto time_dif = std::chrono::duration_cast<std::chrono::milliseconds>(
+                    std::chrono::steady_clock::now() - time_start);
+            // 1s = 1000 milliseconds
+            // 60 frame per second = 1 frame per 16,66  milliseconds
+
+            if (time_dif.count() > 16) {
+                k++;
+                time_start = std::chrono::steady_clock::now();
+                current_key = none;
+                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+                SDL_RenderClear(renderer);
+
+
+                // SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+                for (int i = 0; i < WINDOW_WIDTH * (1 / okres); ++i) {
+                    cord current_pixel_position = {
+                            (int) (i * okres),
+                            (int) (middle + cos(PI * i * 50 * 2) * amplituda)
+                    };
+                    rgb_color current_pixel_color = gen_rainbow(current_pixel_position.y, WINDOW_HEIGHT);
+                    SDL_SetRenderDrawColor(renderer, current_pixel_color.r, current_pixel_color.g,
+                                           current_pixel_color.b, 255);
+
+
+                    //center
+
+                    SDL_RenderDrawPoint(renderer, current_pixel_position.x, current_pixel_position.y);
+
+
+                    // up
+                    SDL_RenderDrawPoint(renderer, current_pixel_position.x, current_pixel_position.y - 2);
+                    SDL_RenderDrawPoint(renderer, current_pixel_position.x, current_pixel_position.y - 1);
+
+                    //down
+                    SDL_RenderDrawPoint(renderer, current_pixel_position.x, current_pixel_position.y + 1);
+                    SDL_RenderDrawPoint(renderer, current_pixel_position.x, current_pixel_position.y + 2);
+                    // left
+                    SDL_RenderDrawPoint(renderer, current_pixel_position.x - 2, current_pixel_position.y);
+                    SDL_RenderDrawPoint(renderer, current_pixel_position.x - 1, current_pixel_position.y);
+                    //right
+                    SDL_RenderDrawPoint(renderer, current_pixel_position.x + 1, current_pixel_position.y);
+                    SDL_RenderDrawPoint(renderer, current_pixel_position.x + 2, current_pixel_position.y);
+                    // corners
+                    SDL_RenderDrawPoint(renderer, current_pixel_position.x - 1, current_pixel_position.y - 1);
+                    SDL_RenderDrawPoint(renderer, current_pixel_position.x - 1, current_pixel_position.y + 1);
+                    SDL_RenderDrawPoint(renderer, current_pixel_position.x + 1, current_pixel_position.y + 1);
+                    SDL_RenderDrawPoint(renderer, current_pixel_position.x + 1, current_pixel_position.y - 1);
+
+
+                }
+                SDL_RenderPresent(renderer);
+            }
+        }
+    }
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 18;
+}
+
+int window_with_line_a(void *ptr_to_data) {
+
+    SDL_Event event;
+    SDL_Renderer *renderer;
+    SDL_Window *window;
+
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer);
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+    for (int i = 0; i < WINDOW_WIDTH; ++i)
+        SDL_RenderDrawPoint(renderer, i, WINDOW_HEIGHT / 2);
+    SDL_RenderPresent(renderer); // starting line
+
+    auto time_start = std::chrono::steady_clock::now();
+
+
+    double amplituda = 150;
+    double okres = 0.5;
+    int k = 0;
+    int middle = WINDOW_HEIGHT / 2;
+
+    while (true) {
+        if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
+            break;
+
+        {
+            auto time_dif = std::chrono::duration_cast<std::chrono::milliseconds>(
+                    std::chrono::steady_clock::now() - time_start);
+            // 1s = 1000 milliseconds
+            // 60 frame per second = 1 frame per 16,66  milliseconds
+
+            if (time_dif.count() > 16) {
+                time_start = std::chrono::steady_clock::now();
+
+
+                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+                SDL_RenderClear(renderer); // clear last frame
+
+                cord current_pixel_position = { // draw dot on the left
+                        0,
+                        WINDOW_HEIGHT / 2
+                };
+                draw_big_point(renderer, current_pixel_position, 6);
+
+                int temp_x = 0; // used to connect first dot to left margin
+                int temp_y = WINDOW_HEIGHT / 2;
+
+
+                for (int i = 0; i < data.size(); i++) {
+
+                    current_pixel_position = {
+                            data[i].x,
+                            data[i].y
+                    };
+                    draw_big_point(renderer, current_pixel_position, 6);
+
+
+
+
+
+
+                     temp_x = data[i].x;
+                     temp_y =  data[i].y;
+                }
+
+                current_pixel_position = {// draw dot on the right
+                        0,
+                        WINDOW_HEIGHT * WINDOW_WIDTH - WINDOW_HEIGHT / 2
+
+                };
+                draw_big_point(renderer, current_pixel_position, 6);
+
+                SDL_RenderPresent(renderer);
+            }
+        }
+    }
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 18;
+}
+
+
+int main(int argc, char *argv[]) {
+
+    int *ptr_to_data;
+    data.push_back({200, 200});
+    data.push_back({200 * 2, 200 * 2});
+    data.push_back({200 * 3, 200 * 3});
+    data.push_back({200 * 4, 200 * 4});
+
+    SDL_Thread *mainthread;
+
+    mainthread = SDL_CreateThread(window_with_line, "idk", (void *) ptr_to_data);
+
+
+    int *thread_status;
+    SDL_WaitThread(mainthread, thread_status);
+
+    printf("Thread returned value: %d\n", *thread_status);
+
+    return 0;
 }
