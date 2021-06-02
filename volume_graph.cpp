7dd40f393f9237ae7b77091d85b3b92f35759589
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-06-03 01:08:54 +0200

    volume graph is showing off, it's a bit weird but it works sooo

---
+++ volume_graph.cpp
@@ -0,0 +1,86 @@
+//
+// Created by piotr233  on 16.04.2021.
+//
+
+
+#include <chrono>
+#include "components/graphics/equalizer_window.h"
+
+
+int main(int argc, char *argv[]) {
+    srand(time(NULL))
+    ;SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
+
+
+    std::mutex surface_guard;
+
+    auto *surface = new canvas(WINDOW_WIDTH, WINDOW_HEIGHT, {0, 0, 0});
+
+    std::thread window(equalizer_window, surface, std::ref(surface_guard)); // thread containing window
+
+    surface->fill({0, 0, 0});
+
+
+    int bar_head_position = 0;
+    int bar_height = 10;/// how many pixels from the bottom the bar will be displayed
+    int line_thickness = 5;
+    std::chrono::milliseconds audio_lenght = std::chrono::milliseconds(60000);
+
+    for (int i = 0; i < WINDOW_WIDTH; i++) {
+
+        int j = 0;
+        for (; j < line_thickness; j++) {
+            surface->draw_point({i, WINDOW_HEIGHT - j - bar_height}, 5, {180, 180, 180});
+        }
+
+    }
+
+    int temporary_high = 0;
+    double trend = 0;
+
+
+    while (bar_head_position != WINDOW_WIDTH) {
+        /// draw current "volume level" for now it's random generated it wll change in the future
+        surface->draw_point({bar_head_position,  WINDOW_HEIGHT/2 - temporary_high}, 4,
+                            gen_rainbow(WINDOW_HEIGHT/2 - temporary_high, WINDOW_HEIGHT));
+
+        /// to make it pretty I draw it emmm like this;
+        /// it's only made to make it pretty
+        surface->draw_point({bar_head_position,  WINDOW_HEIGHT/2 + temporary_high}, 4,
+                            gen_rainbow(WINDOW_HEIGHT/2 + temporary_high, WINDOW_HEIGHT));
+
+
+
+        /// generate change in time
+
+        temporary_high -= trend;
+
+
+        temporary_high = temporary_high / 3  ;
+
+        /// small changes in trend
+        if (bar_head_position % 8 == 2) trend += rand() % 16-8;
+
+        /// very small changes in trend (about the size of penis that mati has to work around)
+         trend += rand() % 8-4;
+
+
+
+
+        /// update line of progress bar
+        for (int i = 0; i < bar_head_position; i++) {
+            int j = 0;
+            for (; j < line_thickness; j++) {
+                surface->draw_point({i, WINDOW_HEIGHT - j - bar_height}, 5, {0, 210, 0});
+            }
+        }
+        std::this_thread::sleep_for(audio_lenght / WINDOW_WIDTH);
+        /// move the barr one pixel to the right;
+        bar_head_position++;
+    }
+
+    window.join();
+
+    return 0;
+}
+
