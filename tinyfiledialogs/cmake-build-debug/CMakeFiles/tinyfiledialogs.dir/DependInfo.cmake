f83d966930fcdca62bb8f6b0a52382fa2c80cc5d
Mateusz Kojro <mateuszkojro@outlook.com>
2021-06-07 15:48:36 +0200

    Adding lib for file dialogs

---
+++ tinyfiledialogs/cmake-build-debug/CMakeFiles/tinyfiledialogs.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/mateu/Documents/tmp/tinyfiledialogs-current/tinyfiledialogs/hello.c" "C:/Users/mateu/Documents/tmp/tinyfiledialogs-current/tinyfiledialogs/cmake-build-debug/CMakeFiles/tinyfiledialogs.dir/hello.c.obj"
+  "C:/Users/mateu/Documents/tmp/tinyfiledialogs-current/tinyfiledialogs/more_dialogs/tinyfd_moredialogs.c" "C:/Users/mateu/Documents/tmp/tinyfiledialogs-current/tinyfiledialogs/cmake-build-debug/CMakeFiles/tinyfiledialogs.dir/more_dialogs/tinyfd_moredialogs.c.obj"
+  "C:/Users/mateu/Documents/tmp/tinyfiledialogs-current/tinyfiledialogs/tinyfiledialogs.c" "C:/Users/mateu/Documents/tmp/tinyfiledialogs-current/tinyfiledialogs/cmake-build-debug/CMakeFiles/tinyfiledialogs.dir/tinyfiledialogs.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../more_dialogs"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tinyfiledialogs/cmake-build-debug/CMakeFiles/tinyfiledialogs.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tinyfiledialogs.dir/hello.c.obj"
+  "CMakeFiles/tinyfiledialogs.dir/more_dialogs/tinyfd_moredialogs.c.obj"
+  "CMakeFiles/tinyfiledialogs.dir/tinyfiledialogs.c.obj"
+  "tinyfiledialogs.exe"
+  "tinyfiledialogs.exe.manifest"
+  "tinyfiledialogs.lib"
+  "tinyfiledialogs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tinyfiledialogs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
