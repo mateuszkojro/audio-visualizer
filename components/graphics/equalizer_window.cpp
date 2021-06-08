728afe1b387f0e6b567f16f38315a1ecfa3f5f84
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-06-09 00:37:03 +0200

    un used functions are gone

--- components/graphics/equalizer_window.cpp
+++ components/graphics/equalizer_window.cpp
@@ -114,42 +114,7 @@ void draw_function(Canvas &surface, std::vector<int> local_values, bool draw_big
 }
 
 
-void
-draw_function_but_fill_it_below(Canvas &surface, std::vector<int> local_values, bool draw_big_points, bool static_color,
-                                bool connect_window_edges) {
 
-    std::vector<Coord> p_positions; // this i thing can be static
-
-    // flipped all values, to make them appear from the bottom of window rather than on top
-
-    for (int &i:local_values) i = WINDOW_HEIGHT - i;
-
-    p_positions = create_points(local_values);
-
-
-    for (unsigned i = 0; i < p_positions.size() - 1; ++i) {
-
-        auto function_between_points = gen_function_between_points(p_positions[i], p_positions[i + 1]);
-
-        for (auto j:function_between_points) {
-
-            if (!static_color) surface.draw_point(j, 3, gen_rainbow(j.y, WINDOW_HEIGHT));
-            else surface.draw_point(j, 3);
-
-            for (int k = j.x; k >= 0; k--) {
-
-                if (!static_color)surface.draw_point({j.y, k}, 1, gen_rainbow(k, WINDOW_HEIGHT));
-                else surface.draw_point({j.y, k}, 1);
-
-            }
-        }
-    }
-
-    if (draw_big_points)
-        for (auto j:p_positions)
-            surface.draw_point(j, 6, gen_rainbow(j.y, WINDOW_HEIGHT));
-
-}
 
 void draw_levels(Canvas &surface, std::vector<int> local_values, bool draw_big_points, bool static_color) {
 
@@ -175,77 +140,7 @@ void draw_levels(Canvas &surface, std::vector<int> local_values, bool draw_big_p
 
 }
 
-extern std::queue<frame> analyzed_bus;
 
-void equalizer_window(Canvas *surface, std::mutex &surface_guard, std::vector<Button> &button_vector) {
-
-
-    SDL_Window *window = SDL_CreateWindow(
-            "lele",                  // window title
-            100,           // initial x position
-            100,           // initial y position
-            WINDOW_WIDTH,                               // width, in pixels
-            WINDOW_HEIGHT,                               // height, in pixels
-            SDL_WINDOW_OPENGL                // flags - see below
-    );
-
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
-
-    SDL_Texture *texture = SDL_CreateTexture(renderer,
-                                             SDL_PIXELFORMAT_RGBA32,
-                                             SDL_TEXTUREACCESS_TARGET,
-                                             WINDOW_WIDTH,
-                                             WINDOW_HEIGHT);
-
-
-    SDL_Event event;
-    auto time_start = std::chrono::steady_clock::now();
-
-
-    while (true) { // main loop
-        if (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) break;
-            if (event.type == SDL_MOUSEBUTTONUP) {
-                int x;
-                int y;
-                SDL_GetMouseState(&x, &y);
-                std::cout << "x: " << x << " y:  " << y;
-                for (int i = 0; i < button_vector.size(); i++)
-                    if (button_vector[i].detect_press({y, x})) std::cout << "   button id:  " << i;
-                std::cout << "\n";
-            }
-        }
-
-        {
-            auto time_dif = std::chrono::duration_cast<std::chrono::milliseconds>(
-                    std::chrono::steady_clock::now() - time_start);
-            // 1s = 1000 milliseconds
-            // 60 frame per second = 1 frame per 16,66  milliseconds
-
-
-            //thread awaits the difference in time
-            // in case that window will be generated and shown in time less than 1 frame, we wait the difference to always generate one frame per 60 s
-            std::this_thread::sleep_for(
-                    std::chrono::milliseconds(16 - std::chrono::duration_cast<std::chrono::milliseconds>(
-                            time_dif).count()));
-
-            time_start = std::chrono::steady_clock::now();
-            {
-                std::lock_guard<std::mutex> guard(surface_guard);
-                SDL_UpdateTexture(texture, NULL, surface->get_pixel_ptr(), surface->pitch());
-
-            }
-            SDL_RenderCopy(renderer, texture, NULL, NULL);
-            SDL_RenderPresent(renderer);
-
-
-        }
-    }
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-}
 
 void equalizer_window_from_data(FourierConfig *data) {
     SDL_Window *window = SDL_CreateWindow(
